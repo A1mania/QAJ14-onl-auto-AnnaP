e5749792fd3d9da3c4cd2557de50e1fe6915766a
Anna Pachkovskaya <106625221+A1mania@users.noreply.github.com>
2025-11-24 17:18:11 +0300

    HW15-16 changed in accordance to review comments

--- src/HW16/hw16.js
+++ src/HW16/hw16.js
@@ -1,42 +1,40 @@
 // 2. У нас поменялись требования к паролю. Юзерам выслана ссылка на смену пароля. Теперь когда они зайдут к нам на сайт, мы должны проверить, что их новый пароль соответствует следующим требованиям:
 //      - минимум 8 символов                            | латинские буквы, максимум 15
-//      - минимум одна заглавная буква      
+//      - минимум одна заглавная буква
 //      - минимум одна цифра
 //      - минимум один специальный символ из набора     | ^!@_$&*()-+
-// а. Создайте функцию, которая будет принимать на вход строку (пароль) и возвращать true, если пароль соответствует требованиям и false, 
+// а. Создайте функцию, которая будет принимать на вход строку (пароль) и возвращать true, если пароль соответствует требованиям и false,
 // если не соответствует.
-// б. Нашим аналитикам интересно, какие цифры чаще всего используются юзерами в паролях. модифицируйте функцию так, 
-// чтобы она вместо булевого значения возвращала объект по следующим примерам: 
+// б. Нашим аналитикам интересно, какие цифры чаще всего используются юзерами в паролях. модифицируйте функцию так,
+// чтобы она вместо булевого значения возвращала объект по следующим примерам:
 //   input: 'Password123!' -> output: { isValid: true, digits: [1, 2, 3] }
 //   input: 'myC00!Pa55w0rd' -> output: { isValid: true, digits: [0, 0, 5, 5, 0] }
 
-function passwordValidate(password){
-    const symbolAmount = /^.{8,15}$/;
-    const hasCapital = /[A-Z]/;
-    const hasInteger = /[0-9]/;
-    const hasSpecial = /[\^!@_$&*()-\+]/;
-    const digits = [];
+function passwordValidate(password) {
+  const symbolAmount = /^.{8,15}$/;
+  const hasCapital = /[A-Z]/;
+  const hasInteger = /[0-9]/;
+  const hasSpecial = /[\^!@_$&*()-\+]/;
+  const validPassword =
+    symbolAmount.test(password) &&
+    hasCapital.test(password) &&
+    hasInteger.test(password) &&
+    hasSpecial.test(password);
+  const digits = [];
 
+  for (let i = 0; i < password.length; i++) {
+    if (password[i].match(/[0-9]/)) {
+      digits.push(password[i]);
+    }
+  }
 
-    if (symbolAmount.test(password) && hasCapital.test(password) 
-    && hasInteger.test(password) && hasSpecial.test(password)){
-        for (let i = 0; i < password.length; i++){
-        if (!isNaN(password[i])){
-            digits.push(password[i]);
-        }
-        }
-        return `isValid: true, digits: [${digits}]`
-    }
-    else{
-        for (let i = 0; i < password.length; i++){
-        if (!isNaN(password[i])){
-            digits.push(password[i]);
-        }
-        }
-        return `false, digits: [${digits}]`
-    }
-    }
- 
+  if (validPassword) {
+    return `isValid: true, digits: [${digits}]`;
+  } else {
+    return `false, digits: [${digits}]`;
+  }
+}
+
 console.log(passwordValidate("Passwor^12"));
 console.log(passwordValidate("Pas1wor"));
 console.log(passwordValidate("Pa*3wor"));
@@ -44,10 +42,10 @@ console.log(passwordValidate("Password1!Password3!"));
 console.log(passwordValidate(""));
 
 // 3. Наши пользователи ранее могли в качестве имени (username) выбрать как произвольное имя так и емейл.
-// Теперь мы хотим убрать возможность использовать просто имя. 
-// Чтобы обработать существубщие данные, создайте функцию, которая будет принимать на вход .json файл 
-// с данными пользователей (username, name, last_name, email) и возвращать массив заготовленных сообщений 
-// для коммуникации с юзерами. 
+// Теперь мы хотим убрать возможность использовать просто имя.
+// Чтобы обработать существубщие данные, создайте функцию, которая будет принимать на вход .json файл
+// с данными пользователей (username, name, last_name, email) и возвращать массив заготовленных сообщений
+// для коммуникации с юзерами.
 // На выходе должен быть объект с данными только по юзерам у которых username не является емейлом.
 // Ожидаемый объект на выходе:
 // {
@@ -69,15 +67,14 @@ const data = readFileSync("src/HW16/fileHW16.json", "utf-8");
 const stringObj = JSON.parse(data);
 
 const isValidEmail = /^[a-zA-Z0-9._+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-//
 const messagesByUser = [];
 
 for (const user of stringObj) {
-  if (isValidEmail.test(user.username) === false) {
+  if (!isValidEmail.test(user.username)) {
     messagesByUser.push({
       [user.username]: {
-        email: user.email,
-        message: `Hello ${user.name} ${user.last_name}, please update your username ${user.username} to be a valid email to comply with our new policy.`,
+        email: user.email ?? "not provided",
+        message: `Hello ${user.name ?? ""} ${user.last_name ?? ""}, please update your username ${user.username} to be a valid email to comply with our new policy.`,
       },
     });
   }
